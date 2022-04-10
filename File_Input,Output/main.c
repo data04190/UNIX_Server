02a09dd48e9f9cba375b260636954f2e26ec9bb8
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-11 00:22:58 +0900

    Add files via upload

---
+++ File_Input,Output/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "student.h"
+#include "dbCreate.h"
+#include "dbQuery.h"
+#include "dbUpdate.h"
+#include "dbList.h"
+#include "dbAdd.h"
+#include "dbDelete.h"
+#include "dbListScore.h"
+
+
+int main(int argc, char *argv[])
+{
+   char c = 'x';
+   int fd, id;
+   student rec,search;
+
+
+    if (argc < 2) {
+        fprintf(stderr,  "사용법 : %s file\n", argv[0]);
+        exit(1);
+    }
+
+
+
+
+
+   while(1){
+
+        printf("무슨 기능을 수행할까요(c/q/u/l/a/d/s/e)? ");
+        scanf("%c", &c);
+        fflush(stdin);
+        //if(( scanf("%c", &c)== 1) && ( c!= NULL)){
+
+
+             if(c == 'c')
+                dbcreate(argv);
+
+             if( c== 'q')
+                dbquery(argv);
+
+             if (c == 'u')
+                dbupdate(argv);
+
+             if (c == 'l')
+                 dblist(argv);
+
+             if (c == 'a')
+                 dbadd(argv);
+
+             if (c == 'd')
+                 dbdelete(argv);
+
+             if (c == 's')
+                 list_score(argv);
+
+             if (c == 'e')
+                break;
+      // }
+
+     }
+
+
+
+
+   //close(fd);
+    exit(0);
+}
