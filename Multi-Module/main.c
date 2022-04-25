c6793987cdf7b6682eb5eb6e5244ec736f1e6014
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-25 14:46:32 +0900

    Update main.c

--- Multi-Module/main.c
+++ Multi-Module/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include "string_tokenize.h"
 #include "len_str.h"
@@ -31,16 +31,4 @@ main()
        len_str(count,sArr);
 
    return 0;
-}
--bash-3.2$ cat copy.c
-/* copy: from을 to에 복사, to가 충분히 크다*/
-void copy(char *from, char *to)
-{
-   int i;
-
-   for(i=0; *(from+i)!='\0'; i++){
-        *(to+i) = *(from+i);
-   }
-   *(to+i) = '\0';
-
 }
