c4683944b9dea3aec9b196edfad4f1e952fb6c17
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-25 14:48:52 +0900

    Update copy.c

--- Multi-Module/copy.c
+++ Multi-Module/copy.c
@@ -10,16 +10,3 @@ void copy(char *from, char *to)
    *(to+i) = '\0';
 
 }
--bash-3.2$
--bash-3.2$ cat copy.c
-/* copy: from을 to에 복사, to가 충분히 크다*/
-void copy(char *from, char *to)
-{
-   int i;
-
-   for(i=0; *(from+i)!='\0'; i++){
-        *(to+i) = *(from+i);
-   }
-   *(to+i) = '\0';
-
-}
