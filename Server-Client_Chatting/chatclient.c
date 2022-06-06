49adb4ad2237e6ff77fe9289137bd2133bddebb2
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-06-06 17:43:17 +0900

    Update chatclient.c

--- Server-Client_Chatting/chatclient.c
+++ Server-Client_Chatting/chatclient.c
@@ -9,22 +9,35 @@
 
 main()
 {
-    int fd1, fd2, n;
-    char inmsg[MAXLINE];
+    int fd1, fd2, n,pid;
+    char smsg[MAXLINE], cmsg[MAXLINE];
 
-    fd1 = open("/home/users/22-1/kmk04190/hw05/chatfifo1", O_RDONLY);   // 절대경로
-    fd2 = open("/home/users/22-1/kmk04190/hw05/chatfifo2", O_WRONLY);   //절대경로
+    fd1 = open("/home/users/22-1/kmk04190/hw05/chatfifo1", O_RDONLY);
+    fd2 = open("/home/users/22-1/kmk04190/hw05/chatfifo2", O_WRONLY);
     if(fd1 == -1 || fd2 == -1) {
         perror("open");
         exit(1);
-    }
+    }  
 
-    printf("클라이언트 시작 \n");
-    while(1) {
-        n = read(fd1, inmsg, MAXLINE);
-        printf("서버 -> %s\n", inmsg);
-        printf("클라이언트 : ");
-        fgets(inmsg, MAXLINE, stdin);
-        write(fd2, inmsg, strlen(inmsg)+1);
+   printf("클라이언트 시작 \n");
+
+   pid = fork(); 
+
+    while(1) { 
+
+        if (pid == 0) {    //자식 프로세스 
+
+	         n = read(fd1, smsg, MAXLINE);
+             if ( n > 0) 
+                 printf("서버 :  %s\n", smsg);
+
+         } else{           // 부모 프로세스
+
+	         fgets(cmsg, MAXLINE, stdin);
+	         if ( (strlen(cmsg) > 1)  ) {
+                  printf("클라이언트 : %s", cmsg);
+	              write(fd2, cmsg, strlen(cmsg)+1);
+             }
+       }
     }
 }
