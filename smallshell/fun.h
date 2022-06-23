40e86cdea45234af87cc5a3321786b8e34a9915e
MyoKyeong Kim <77683645+data04190@users.noreply.github.com>
2022-06-23 10:54:57 +0900

    Delete fun.h

--- smallshell/fun.h
+++
@@ -1,6 +0,0 @@
-
-void fun1(char *arg[],int argi);
-void fun2(char *arg[], int argi);
-void fun3(char *arg[], int argi, int *fg);
-void fun4(char *arg[], int argi);
-void fun5(char *arg[], int argi);
