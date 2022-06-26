54ec9a585d6da067a35dc359541b84bd34cc2dc1
MyoKyeong Kim <77683645+data04190@users.noreply.github.com>
2022-06-26 10:03:19 +0900

    Add files via upload

---
+++ File_Input,Output/DBFunction.h
@@ -0,0 +1,10 @@
+
+#define BUFSIZE 512
+
+void dbadd( char *argv[]);
+void dbcreate(char *argv[]);
+void dbdelete(char *argv[]);
+void dblist( char *argv[]);
+void list_score ( char *argv[]);
+void dbquery (char *argv[]);
+void dbupdate( char *argv[]);
